202bae1d92c0173599fe9efbb325ba73451c6f3e
miusoph <123985948+miuxsoph@users.noreply.github.com>
2023-10-25 14:47:46 -0700

    Add files via upload

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+use std::thread::sleep;
+use std::time::Duration;
+
+fn main() {
+    let mut q = String::from("100100100100100");
+    let mut r = String::from("101110101011011101001110101110101110100000");
+    let w = 3;
+
+    while q.len() > w {
+        if r.chars().next() == Some('0') {
+            q = q.chars().skip(1).collect();
+        } else {
+            r = r.chars().skip(1).chain(r.chars().take(1)).collect();
+            if q.chars().next() == Some('1') {
+                q.push(r.chars().next().unwrap());
+            }
+        }
+        r = r.chars().skip(1).chain(r.chars().take(1)).collect();
+        println!("{}", q);
+        sleep(Duration::from_secs_f32(0.03));
+    }
+}
